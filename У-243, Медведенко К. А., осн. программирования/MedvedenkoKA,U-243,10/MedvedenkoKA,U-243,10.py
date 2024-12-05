00a8c61c689edfcd85b5bd09b0418a2cebfd76da
MedvedenkoU243 <medvedenko604@mail.ru>
2024-12-05 23:46:34 +0300

    Add files via upload

---
+++ У-243, Медведенко К. А., осн. программирования/MedvedenkoKA,U-243,10/MedvedenkoKA,U-243,10.py
@@ -0,0 +1,31 @@
+#1
+import numpy as np
+m = np.loadtxt("У-243,Медведенко К. А., осн. программирования/MedvedenkoKA,U-243,10/Медведенко_Ксения_Александровна_У-243_vvod.txt", dtype='i', delimiter=',')
+with open('У-243,Медведенко К. А., осн. программирования/MedvedenkoKA,U-243,10/Медведенко_Ксения_Александровна_У-243_vvod.txt', 'r') as file:
+    n = sum(1 for line in file)
+for i in range(n):
+    for j in range(i, n):
+        m[j][i] = m[i][j]
+for row in m:
+    print(row, sep='\t')
+#2
+m = np.loadtxt("У-243,Медведенко К. А., осн. программирования/MedvedenkoKA,U-243,10/Медведенко_Ксения_Александровна_У-243_vvod.txt", dtype='i', delimiter=',')
+matrix=np.array(m)
+diagonal_elements=np.diagonal(matrix)
+trace=np.sum(diagonal_elements)
+for i in range(len(matrix)):
+    if i%2==0:
+        matrix[i]=matrix[i]/trace
+print("Диагональные элементы матрицы:", diagonal_elements)
+print("След матрицы:", trace)
+print("Преобразованная матрица:")
+print(matrix)
+d_e=str(diagonal_elements)
+t=str(trace)
+mat=str(matrix)
+with open("У-243,Медведенко К. А., осн. программирования/MedvedenkoKA,U-243,10/Медведенко_Ксения_Александровна_У-243_vivod.txt", "w") as file:
+    file.write(d_e)
+    file.write("\n")
+    file.write(t)
+    file.write("\n")
+    file.write(mat)
